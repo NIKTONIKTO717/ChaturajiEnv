a1963f2c7a66f7e4f1467039a4e2bdcdd00b5dea
Samuel Krempaský <36561335+NIKTONIKTO717@users.noreply.github.com>
2025-03-03 15:53:48 +0100

    pyd update

--- ChaturajiEnv/ChaturajiEnv.cpp
+++ ChaturajiEnv/ChaturajiEnv.cpp
@@ -1539,16 +1539,16 @@ struct game_storage {
         float sum = red + blue + yellow + green;
         float bounds[4];
         bounds[0] = red / sum;
-        bounds[1] = (bounds[0] + blue) / sum;
-        bounds[2] = (bounds[1] + yellow) / sum;
-        bounds[3] = (bounds[2] + green) / sum;
+        bounds[1] = bounds[0] + (blue / sum);
+        bounds[2] = bounds[1] + (yellow / sum);
+        bounds[3] = bounds[2] + (green / sum);
 
         double target = dist(global_rng());
         uint turn = 0;
 
-        for (uint i = 3; i >= 0; i--) {
-            if (target <= bounds[i]) {
-                turn = i;
+        for (uint i = 4; i > 0; i--) {
+            if (target <= bounds[i-1]) {
+                turn = i-1;
             }
         }
         while (true) { //sample until sampled one with last turn with specific color.
@@ -1559,16 +1559,16 @@ struct game_storage {
             py::buffer_info input_buf = input.request();
             float* input_ptr = static_cast<float*>(input_buf.ptr);
 
-            auto& out_v = std::get<2>(sample);
+            /*auto& out_v = std::get<2>(sample);
             py::buffer_info out_v_buf = out_v.request();
-            float* out_v_ptr = static_cast<float*>(out_v_buf.ptr);
+            float* out_v_ptr = static_cast<float*>(out_v_buf.ptr);*/
 
             if (input_ptr[1280 * T + 512 + turn * 64] == 1.0f) {
 
                 std::cout << "turn: " << turn << ", layers: [" << input_ptr[1280 * T + 512 + 0 * 64] << ", " << input_ptr[1280 * T + 512 + 1 * 64] << ", " << input_ptr[1280 * T + 512 + 2 * 64] << ", " << input_ptr[1280 * T + 512 + 3 * 64] << "]";
                 std::cout << ", scores: [" << input_ptr[1280 * T + 0 * 64] << ", " << input_ptr[1280 * T + 1 * 64] << ", " << input_ptr[1280 * T + 2 * 64] << ", " << input_ptr[1280 * T + 3 * 64] << "]";
                 std::cout << ", active: [" << input_ptr[1280 * T + 256 + 0 * 64] << ", " << input_ptr[1280 * T + 256 + 1 * 64] << ", " << input_ptr[1280 * T + 256 + 2 * 64] << ", " << input_ptr[1280 * T + 256 + 3 * 64] << "]";
-                std::cout << ", V: [" << out_v_ptr[0] << ", " << out_v_ptr[1] << ", " << out_v_ptr[2] << ", " << out_v_ptr[3] << "]";
+                //std::cout << ", V: [" << out_v_ptr[0] << ", " << out_v_ptr[1] << ", " << out_v_ptr[2] << ", " << out_v_ptr[3] << "]";
                 std::cout << ", random: " << target << "\n";
                 return sample;
             }
