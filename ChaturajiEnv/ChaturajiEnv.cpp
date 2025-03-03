f3502ae0d68097ac3959ca576c1e5b0102c8d9d6
Samuel Krempaský <36561335+NIKTONIKTO717@users.noreply.github.com>
2025-03-03 15:47:59 +0100

    pyd update

--- ChaturajiEnv/ChaturajiEnv.cpp
+++ ChaturajiEnv/ChaturajiEnv.cpp
@@ -1546,7 +1546,7 @@ struct game_storage {
         double target = dist(global_rng());
         uint turn = 0;
 
-        for (uint i = 0; i < 4; i++) {
+        for (uint i = 3; i >= 0; i--) {
             if (target <= bounds[i]) {
                 turn = i;
             }
@@ -1564,10 +1564,12 @@ struct game_storage {
             float* out_v_ptr = static_cast<float*>(out_v_buf.ptr);
 
             if (input_ptr[1280 * T + 512 + turn * 64] == 1.0f) {
+
                 std::cout << "turn: " << turn << ", layers: [" << input_ptr[1280 * T + 512 + 0 * 64] << ", " << input_ptr[1280 * T + 512 + 1 * 64] << ", " << input_ptr[1280 * T + 512 + 2 * 64] << ", " << input_ptr[1280 * T + 512 + 3 * 64] << "]";
                 std::cout << ", scores: [" << input_ptr[1280 * T + 0 * 64] << ", " << input_ptr[1280 * T + 1 * 64] << ", " << input_ptr[1280 * T + 2 * 64] << ", " << input_ptr[1280 * T + 3 * 64] << "]";
-                std::cout << ", active: [" << input_ptr[1280 * T + 64 + 0 * 64] << ", " << input_ptr[1280 * T + 64 + 1 * 64] << ", " << input_ptr[1280 * T + 64 + 2 * 64] << ", " << input_ptr[1280 * T + 64 + 3 * 64] << "]";
-                std::cout << ", V: [" << out_v_ptr[0] << ", " << out_v_ptr[1] << ", " << out_v_ptr[2] << ", " << out_v_ptr[3] << "]\n";
+                std::cout << ", active: [" << input_ptr[1280 * T + 256 + 0 * 64] << ", " << input_ptr[1280 * T + 256 + 1 * 64] << ", " << input_ptr[1280 * T + 256 + 2 * 64] << ", " << input_ptr[1280 * T + 256 + 3 * 64] << "]";
+                std::cout << ", V: [" << out_v_ptr[0] << ", " << out_v_ptr[1] << ", " << out_v_ptr[2] << ", " << out_v_ptr[3] << "]";
+                std::cout << ", random: " << target << "\n";
                 return sample;
             }
         }
