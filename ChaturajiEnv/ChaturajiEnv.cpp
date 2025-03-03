5d75e1e9c0b8ec69fc6098233fce50ee68e732f6
Samuel Krempaský <36561335+NIKTONIKTO717@users.noreply.github.com>
2025-03-03 16:19:00 +0100

    pyd update

--- ChaturajiEnv/ChaturajiEnv.cpp
+++ ChaturajiEnv/ChaturajiEnv.cpp
@@ -1560,17 +1560,17 @@ struct game_storage {
             py::buffer_info input_buf = input.request();
             float* input_ptr = static_cast<float*>(input_buf.ptr);
 
-            /*auto& out_v = std::get<2>(sample);
+            auto& out_v = std::get<2>(sample);
             py::buffer_info out_v_buf = out_v.request();
-            float* out_v_ptr = static_cast<float*>(out_v_buf.ptr);*/
+            float* out_v_ptr = static_cast<float*>(out_v_buf.ptr);
 
             if (input_ptr[1280 * T + 512 + turn * 64] == 1.0f) {
 
-                /*std::cout << "turn: " << turn << ", layers: [" << input_ptr[1280 * T + 512 + 0 * 64] << ", " << input_ptr[1280 * T + 512 + 1 * 64] << ", " << input_ptr[1280 * T + 512 + 2 * 64] << ", " << input_ptr[1280 * T + 512 + 3 * 64] << "]";
+                std::cout << "turn: " << turn << ", layers: [" << input_ptr[1280 * T + 512 + 0 * 64] << ", " << input_ptr[1280 * T + 512 + 1 * 64] << ", " << input_ptr[1280 * T + 512 + 2 * 64] << ", " << input_ptr[1280 * T + 512 + 3 * 64] << "]";
                 std::cout << ", scores: [" << input_ptr[1280 * T + 0 * 64] << ", " << input_ptr[1280 * T + 1 * 64] << ", " << input_ptr[1280 * T + 2 * 64] << ", " << input_ptr[1280 * T + 3 * 64] << "]";
                 std::cout << ", active: [" << input_ptr[1280 * T + 256 + 0 * 64] << ", " << input_ptr[1280 * T + 256 + 1 * 64] << ", " << input_ptr[1280 * T + 256 + 2 * 64] << ", " << input_ptr[1280 * T + 256 + 3 * 64] << "]";
-                //std::cout << ", V: [" << out_v_ptr[0] << ", " << out_v_ptr[1] << ", " << out_v_ptr[2] << ", " << out_v_ptr[3] << "]";
-                std::cout << ", random: " << target << "\n";*/
+                std::cout << ", V: [" << out_v_ptr[0] << ", " << out_v_ptr[1] << ", " << out_v_ptr[2] << ", " << out_v_ptr[3] << "]";
+                std::cout << ", random: " << target << "\n";
                 return sample;
             }
         }
