b42eac459c13e7df3ae4ff2171a202a6331068bd
Samuel Krempaský <36561335+NIKTONIKTO717@users.noreply.github.com>
2025-05-05 19:18:50 +0200

    self-play script

---
+++ x64/Release/compare_loop_vs_vanilla_mcts.py
@@ -0,0 +1,88 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import faulthandler
+from network import AlphaZeroNet
+from comparison import Compare
+import pickle
+faulthandler.enable()
+
+CPU_CORES = 22                  # Number of CPU cores to use for MCTS
+BUDGET = [400, 800, 1600, 3200] # Number of MCTS simulations per move (800 in AlphaZero)
+EVAL_GAMES = 1                  # Number of games to evaluate per CPU core    
+
+# folders of 4 different models
+AGENTS_DIR = [
+    'models_no_eval',
+    None,
+    None,
+    None,
+]
+# names of pickle files, same for each agent
+PICKLE_FILES = [
+    'acting_net_0',
+    'acting_net_1',
+    'acting_net_2',
+    'acting_net_3',
+    'acting_net_4',
+    'acting_net_5',
+    'acting_net_6',
+    'acting_net_7',
+    'acting_net_8',
+    'acting_net_9',
+    'acting_net_10',
+    'acting_net_11',
+    'acting_net_12',
+]
+
+def main():
+    rewards_gens_array = []
+    moves_sum_array = []
+    score_sum_array = []
+    for file in PICKLE_FILES:
+        start_time = time.time()
+        networks = []
+        rewards_sum_sum = [0,0,0,0]
+        score_sum_sum = [0,0,0,0]
+        for i in range(4):
+            if AGENTS_DIR[i] is None:
+                networks.append(None)
+            else:
+                networks.append(AlphaZeroNet((173,8,8), 4096, 8, 32, 32))
+                networks[i].load_state_dict(torch.load(AGENTS_DIR[i] + '/' + file + '.pickle'))
+        #networks[3] = None # last one is MCTS
+        compare = Compare(networks, AGENTS_DIR, file, CPU_CORES, EVAL_GAMES)
+        for setup in [(0,1,2,3), (0,1,3,2), (0,2,1,3), (0,2,3,1), (0,3,1,2), (0,3,2,1)]:
+            print('Comparing agents:', setup)
+            budget_setup = [BUDGET[i] for i in setup]
+            moves_sum, score_sum, rewards_sum, rank_counts = compare.run_shifting(budget_setup, setup)
+            moves_sum_array.append(moves_sum)
+            rewards_sum_reordered = [rewards_sum[setup.index(i)] for i in range(4)]
+            score_sum_reordered = [score_sum[setup.index(i)] for i in range(4)]
+            score_sum_sum = np.add(score_sum_sum, score_sum_reordered)
+            rewards_sum_sum = np.add(rewards_sum_sum, rewards_sum_reordered)
+        rewards_gens_array.append(rewards_sum_sum)
+        score_sum_array.append(score_sum_sum)
+        print('Time taken:', time.time() - start_time)
+
+    with open('vs_vanilla_mcts_rewards_gens_array.pkl', 'wb') as f:
+        pickle.dump(rewards_gens_array, f)
+    with open('vs_vanilla_mcts_moves_sum_array.pkl', 'wb') as f:
+        pickle.dump(moves_sum_array, f)
+    with open('vs_vanilla_mcts_score_sum_array.pkl', 'wb') as f:
+        pickle.dump(score_sum_array, f)
+
+    rewards_by_agent = list(zip(*rewards_gens_array))
+    plt.plot(rewards_by_agent[0], label='no_eval')
+    plt.plot(rewards_by_agent[1], label='MCTS, budget=12800')
+    plt.plot(rewards_by_agent[2], label='MCTS, budget=25600')
+    plt.plot(rewards_by_agent[3], label='MCTS, budget=51200')
+    plt.xlabel('Hours of training')
+    plt.ylabel('Rewards')
+    plt.title('Different agents rewards')
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ x64/Release/self_play.py
@@ -0,0 +1,54 @@
+import torch
+import time
+import faulthandler
+from network import AlphaZeroNet
+from mcts import MCTS 
+import tools
+import os
+faulthandler.enable()
+
+# === Hyperparameters ===
+CPU_CORES = 4              # Number of CPU cores to use for MCTS
+BUDGET = 40                # Number of MCTS simulations per move (800 in AlphaZero)
+SELF_PLAY_DIR = ['cache_games'] # Directory for self-play games
+MODEL_DIR = 'models' # Directory for model storage
+CHECK_INTERVAL = 60  # seconds
+
+def get_latest_model_path(model_dir):
+    models = [f for f in os.listdir(model_dir) if f.endswith(".pkl")]
+    if not models:
+        return None
+    models.sort(key=lambda f: os.path.getmtime(os.path.join(model_dir, f)), reverse=True)
+    print(models)
+    return os.path.join(model_dir, models[0])
+
+def main():
+    device_acting = (torch.device('cpu'))
+    # 20 * 8 + 4 + 4 + 4 + 1 = 173
+    net_acting = AlphaZeroNet((173,8,8), 4096, 8, 32, 32).to(device_acting)
+    net_acting.eval()
+
+    mcts = MCTS(net_acting, 1, CPU_CORES, BUDGET, SELF_PLAY_DIR)
+    mcts.use_model = True
+
+    last_loaded_model = None
+
+    while True:
+        latest_model = get_latest_model_path(MODEL_DIR)
+        if latest_model and latest_model != last_loaded_model:
+            mcts.stop(False)
+            latest_model = get_latest_model_path(MODEL_DIR) # meanwhile another model could be saved
+            print(f"New model detected: {latest_model}")
+            try:
+                state_dict = torch.load(latest_model, map_location="cpu")
+                net_acting.load_state_dict(state_dict)
+                last_loaded_model = latest_model
+                print("Model updated to:", tools.get_model_hash(net_acting))
+            except Exception as e:
+                print(f"Error loading model: {e}")
+            mcts.start(False)
+
+        time.sleep(CHECK_INTERVAL)
+
+if __name__ == '__main__':
+    main()
